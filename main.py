489cb1f1e83b60e9307562f1f01f40c7f1c2edcd
hooman734 <hesamyan@outlook.com>
2023-02-15 00:43:22 +0400

    some fixes including added more api methods and path and exception handlers

--- main.py
+++ main.py
@@ -1,56 +1,88 @@
-from typing import Union, NewType
-from fastapi import FastAPI
+from typing import Union, Optional
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uuid
 
 
 class Book(BaseModel):
-    title: str
     id: Union[str, None] = None
+    title: str
+    description: Optional[str]
 
 books = []
 
 app = FastAPI()
 
+# Welcome Note
 @app.get("/")
 async def root():
-    return { "message": "Welcome to my Book App API"}
+    return { "message": "Welcome to my Book App API" }
 
+# Get all books
 @app.get("/api/v1/books")
 async def show_all():
-    return books
+    return { "all_books": books }
 
+# Get a book by ID - path
 @app.get("/api/v1/book/{id}")
 async def show_by_id(id: str):
-    result = None
     for book in books:
         if str(book.id) == str(id):
-            result = book
-    return result
+            return { "requested_book": book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
 
+# Get a book by ID - query parameter
+@app.get("/api/v1/book")
+async def show_by_id_query(id: str):
+    for book in books:
+        if str(book.id) == str(id):
+            return { "requested_book": book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
+
+# Add new book
 @app.post("/api/v1/book")
 async def add_book(new_book: Book):
-    resul = None
     if new_book.id == None:
         new_book.id = uuid.uuid4().hex
         books.append(new_book)
-        result = new_book
-    return result
+        return { "added_book": new_book }
+    raise HTTPException(418, f"You are not allowed to specify an ID")
 
+# Delete a book
 @app.delete("/api/v1/book/{id}")
 async def delete_by_id(id: str):
-    result = None
     for book in books:
         if str(book.id) == str(id):
-            result = book
-            books.remove(result)
-    return result
+            books.remove(book)
+            return { "deleted_book": book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
 
-@app.put("/api/v1/book/{id}")
+# Edit a book - just title with query parameter
+@app.patch("/api/v1/book/{id}")
 async def edit_by_id(id: str, new_title: str):
-    result = None
     for book in books:
         if str(book.id) == str(id):
-            result = book
             book.title = new_title
-    return result
+            return { "edited_book": book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
+
+# Edit a book - just description with query parameter
+@app.patch("/api/v1/book/{id}")
+async def edit_by_id(id: str, new_description: str):
+    for book in books:
+        if str(book.id) == str(id):
+            book.description = new_description
+            return { "edited_book": book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
+
+# Edit a book - just title with query parameter
+@app.put("/api/v1/book/{id}")
+async def edit_by_id(id: str, new_book: Book):
+    for book in books:
+        if str(book.id) == str(id):
+            books.remove(book)
+            new_book.id = id
+            books.append(new_book)
+            return { "edited_book": new_book }
+    raise HTTPException(404, f"Book with ID: {id} not found!")
+
