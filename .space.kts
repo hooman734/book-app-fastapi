e7b0858d49a0365088d7b5f1744ee96d2ed6dbaa
Hooman Hesamyan <hesamyan@outlook.com>
2023-02-19 18:24:57 +0000

    new

--- .space.kts
+++ .space.kts
@@ -1,15 +1,25 @@
-job("Docker Hub - fastapi") {
-    container(displayName = "docker", image = "docker:latest") {
-      	env["USER"] = Params("dockerhub-username")
-        env["PASS"] = Secrets("dockerhub-password")
-            
+job("Publish to Docker Hub") {
+    host("Build artifacts and a Docker image") {
+        // assign project secrets to environment variables
+        env["HUB_USER"] = Params("dockerhub-username")
+        env["HUB_TOKEN"] = Secrets("dockerhub-password")
+
         shellScript {
-            content = "docker login -u ${'$'}USER -p ${'$'}PASS
+            // login to Docker Hub
+            content = """
+                docker login --username ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
+            """
         }
 
-        service("docker:dind") {
-            alias("dind")
-          
+        dockerBuildPush {
+            labels["vendor"] = "mycompany"
+            tags {
+                +"myrepo/hello-from-space:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
+            content = """
+            	docker build . --file Dockerfile --tag fastapi:$(date+%s)
+             	docker push fastapi:$(date+%s)
+            """
         }
     }
 }
