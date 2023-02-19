79461ef332b1b2e40291f2f88fb907990e57b07c
Hooman Hesamyan <hesamyan@outlook.com>
2023-02-19 18:08:46 +0000

    end modified

--- .space.kts
+++ .space.kts
@@ -1,9 +1,9 @@
 job("Docker Hub - fastapi") {
     container(displayName = "docker", image = "docker:latest") {
         shellScript {
-          	env["USER"] = "hooman777"
-          	env["PASS"] = "$Shima734kk"
-            content = "docker login -u $USER -p $PASS"
+          	env["USER"] = Params("dockerhub-username")
+          	env["PASS"] = Secrets("dockerhub-password")
+            content = "docker login -u ${'$'}USER -p ${'$'}PASS
         }
 
         service("docker:dind") {
